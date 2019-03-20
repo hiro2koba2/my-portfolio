8c19e66ad7141f30ec5856f653baf4e2cbb24608
roffet <kroffet.kroffet@gmail.com>
2019-03-20 14:53:23 +0900

    add:right-navigation-drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,27 +11,49 @@
 					<v-btn flat class="ml-4"><i class="fas fa-envelope fa-2x mr-2"></i>Contact</v-btn>
 				</v-toolbar-items>
 				<v-spacer></v-spacer>
-				<v-toolbar-side-icon class="hidden-md-and-up mr-3"></v-toolbar-side-icon>
+				<v-toolbar-side-icon class="hidden-md-and-up mr-3" @click.stop="rightDrawer = !rightDrawer"></v-toolbar-side-icon>
 			</v-toolbar>
-
-			<!-- <v-navigation-drawer app v-model="drawer" class="primary hidden-md-and-up" >
-				<v-list>
-					<v-list-title>
-						<v-list-tile-action>
-							<v-icon class="white--text">dashboard</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title class="white--text">Dashboard</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-title>
-				</v-list>
-			</v-navigation-drawer> -->
 		</nav>
 		<v-content id="top">
 			<v-container>
 				<nuxt/>
 			</v-container>
 		</v-content>
+
+		<v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-tile>
+					<v-list-tile-action>
+						<i class="fas fa-code fa-1x ma-2"></i>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            Works
+          </v-list-tile-content>
+        </v-list-tile>
+
+				<v-list-tile>
+					<v-list-tile-action>
+						<i class="fas fa-user fa-1x ma-2"></i>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            About
+          </v-list-tile-content>
+				</v-list-tile>
+
+				<v-list-tile>
+					<v-list-tile-action>
+						<i class="fas fa-envelope fa-1x ma-2"></i>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            Contact
+          </v-list-tile-content>
+				</v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+		<v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
 	</v-app>
 </template>
 
@@ -39,12 +61,27 @@
 export default {
 	data() {
 		return {
-			scrollOpt: {
-        duration: 1500,
-        offset: 10,
-        easing: "easeInOutCubic"
-      }
-			// drawer: true
+			miniVariant: false,
+			right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+			items: [
+        {
+          icon: "apps",
+          title: "Top",
+          to: "/"
+        },
+        {
+          icon: "person",
+          title: "About",
+          to: "/about"
+        },
+        {
+          icon: "history",
+          title: "History",
+          to: "/history"
+				}
+			]
 		}
 	}
 }
