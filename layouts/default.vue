c6d67429f3bf68877e4abf3f969bba0a9430883e
roffet <kroffet.kroffet@gmail.com>
2019-03-27 00:55:55 +0900

    fix:フッター表示修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,16 +37,10 @@
       </v-list>
     </v-navigation-drawer>
 
-    <!-- <v-footer class="white--text" color="primary lighten-1">
-      <v-layout justify-center row wrap>
-        <v-btn v-for="link in links" :key="link.text" router :to="link.route" color="white">
-          {{ link.text }}
-        </v-btn>
-        <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
-          &copy;2019 — <strong>roffet</strong>
-        </v-flex>
-      </v-layout>
-    </v-footer> -->
+    <v-footer class="pa-4 white--text" color="primary lighten-1">
+      <v-spacer></v-spacer>
+      &copy;2019 — <strong>roffet</strong>
+    </v-footer>
   </v-app>
 </template>
 
