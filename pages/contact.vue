3a5a3e5f8fbc8cfb8730e27ca215e6dc4187db1d
roffet <kroffet.kroffet@gmail.com>
2019-04-21 22:00:57 +0900

    add: 微調整

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,9 @@
     <h1 class="text-xs-center grey--text">
       Contact
     </h1>
-    <a href="https://github.com/roffet" target="_blunk" class="text-xs-center"><i class="fab fa-github fa-3x" /></a>
+    <div class="text-xs-center my-2 grey--text">
+      <a href="https://github.com/roffet" target="_blunk" class="text-xs-center"><i class="fab fa-github fa-3x" /></a>
+      <p>github</p>
+    </div>
   </v-container>
 </template>
